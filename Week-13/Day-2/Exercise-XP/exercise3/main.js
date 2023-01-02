561dc39bbdb045eae29ac2ff3dff2d46aeef5cc6
ZevLipsky <zevilipsky@gmail.com>
2023-01-02 11:58:49 +0200

    exercise xp

---
+++ Week-13/Day-2/Exercise-XP/exercise3/main.js
@@ -0,0 +1,8 @@
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+
+console.log(x);
+console.log(c + x);
---
+++ Week-13/Day-2/Exercise-XP/npm-node/main.js
@@ -0,0 +1,6 @@
+const a = 5;
+const b = 10;
+
+console.log(`Hello you are ${a + b}`);
+
+//Exercise 1 and 2 are together
